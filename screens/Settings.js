d816fc7694b9dc5d0dcf1d1399d77beaba6b1d41
cheymonty <45781686+cheymonty@users.noreply.github.com>
2021-01-17 00:41:36 -0500

    working on button in settings

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,6 +1,6 @@
 import React, {useContext} from 'react'
-import {StyleSheet, View} from 'react-native'
-import {Divider, Switch, List, Menu, Button} from 'react-native-paper'
+import {StyleSheet, View, TouchableOpacity} from 'react-native'
+import {Divider, Switch, List, Menu, Button, Subheading, IconButton} from 'react-native-paper'
 import {timeToString} from '../utils/helpers'
 import Header from '../components/Header'
 
@@ -62,7 +62,7 @@ export default function Settings() {
                     <Menu
                         visible={timeMenu}
                         onDismiss={closeTimeMenu}
-                        anchor={<Button icon="arrow-down" onPress={() => setTimeMenu(!timeMenu)} color={theme.accent} contentStyle={{flexDirection: "row-reverse"}}>{timeToString(timeOfNotification)}</Button>}
+                        anchor={<Button onPress={() => setTimeMenu(!timeMenu)} color={theme.accent} contentStyle={{flexDirection: "row-reverse"}}>{timeToString(timeOfNotification)}</Button>}
                     >
                         <Menu.Item onPress={() => {setTime(6)}} title="6:00 AM"/>
                         <Menu.Item onPress={() => {setTime(9)}} title="9:00 AM"/>
@@ -72,6 +72,27 @@ export default function Settings() {
                         <Menu.Item onPress={() => {setTime(18)}} title="6:00 PM"/>
                     </Menu>        
                 }
+
+                // TODO: work in progress
+                // right={_ => 
+                //     <Menu
+                //         visible={timeMenu}
+                //         onDismiss={closeTimeMenu}
+                //         anchor={
+                //             <TouchableOpacity onPress={() => setTimeMenu(!timeMenu)} style={{flexDirection: "row"}}>
+                //                 <Subheading style={{color: theme.accent}}>{timeToString(timeOfNotification)}</Subheading>
+                //                 <IconButton icon="arrow-down" color={theme.accent} size={15}/>
+                //             </TouchableOpacity>
+                //         }
+                //     >
+                //         <Menu.Item onPress={() => {setTime(6)}} title="6:00 AM"/>
+                //         <Menu.Item onPress={() => {setTime(9)}} title="9:00 AM"/>
+                //         <Divider />
+                //         <Menu.Item onPress={() => {setTime(12)}} title="12:00 PM"/>
+                //         <Menu.Item onPress={() => {setTime(15)}} title="3:00 PM"/>
+                //         <Menu.Item onPress={() => {setTime(18)}} title="6:00 PM"/>
+                //     </Menu>
+                // }
             />
 
 
