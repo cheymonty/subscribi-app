d0b82a01d3ec028a1fed41c312bc0e80ffdf47a8
cheymonty <45781686+cheymonty@users.noreply.github.com>
2021-01-17 11:52:45 -0500

    working on storage

--- utils/storage.js
+++ utils/storage.js
@@ -1,11 +1,14 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-async function updateAsyncDarkMode(darkMode) {
-    await AsyncStorage.setItem("@darkMode", JSON.stringify(darkMode))
-}
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 async function updateStorage(key, value) {
     await AsyncStorage.setItem(key, JSON.stringify(value))
 }
 
-export {updateAsyncDarkMode, updateStorage}
+async function getStorage(key, dfault) {
+    let v = await AsyncStorage.getItem(key)
+    if (JSON.parse(v))
+        return JSON.parse(v)
+    return dfault
+}
+
+export {updateStorage, getStorage}
